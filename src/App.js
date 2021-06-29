d4d5d3b469d424c77f9a480e1db1f73f6fb15bd1
Akanksha priya <58500059+Spring-dot@users.noreply.github.com>
2021-06-29 17:51:44 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import CallPage from './Components/Callpage';
+import Homepage from './Components/Homepage';
+import NoMatch from './Components/NoMatch';
+import './App.scss';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/:id">
+          <CallPage />
+
+        </Route>
+        <Route exact path="/">
+          <Homepage />
+        </Route>
+        <Route path="*">
+          <NoMatch />
+        </Route>
+
+      </Switch>
+
+    </Router>
+    
   );
 }
 
